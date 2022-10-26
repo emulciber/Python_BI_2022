4126f6520fdc608446ebcb4d25c2eeb6016aa629
e_mulciber <aaren183@gmail.com>
2022-10-26 15:31:51 +0200

    Homework 4 for task review

---
+++ homework_4_numpy/README.md
@@ -0,0 +1,10 @@
+# Functions for numpy array operations
+
+File numpy_challenge.py contains some functions to operate with numpy arrays and matrices.
+
+* `matrix_multiplication` takes two matrices and multiply them.
+* `multiplication_check` takes list of matrices and checks the possibility of successive multiplication.
+* `multiply_matrices` takes list of matrices and multiplies them sequentially.
+* `compute_2d_distance` takes two arrays (with 2 numbers in array) and computes distance between them.
+* `compute_multidimensional_distance` takes two arrays and computes distance between them.
+* `compute_pair_distances` takes array with some rows and returns matrix with pair distances between each two rows.
--- homework_4_numpy/numpy_challenge.py
+++ homework_4_numpy/numpy_challenge.py
@@ -1,25 +1,65 @@
 import numpy as np
 
 
+def matrix_multiplication(matrix_1, matrix_2):
+    return matrix_1 @ matrix_2
+
+
+def multiplication_check(matrix_list):
+    last_matrix_shape = matrix_list[0].shape[1]
+    for i in range(1, len(matrix_list)):
+        if last_matrix_shape != matrix_list[i].shape[0]:
+            return False
+        last_matrix_shape = matrix_list[i].shape[1]
+    return True
+
+
+def multiply_matrices(matrix_list):
+    try:
+        last_matrix = matrix_list[0]
+        for i in range(1, len(matrix_list)):
+            last_matrix = last_matrix @ matrix_list[i]
+        return last_matrix
+    except:
+        return None
+
+
+def compute_2d_distance(arr1, arr2):
+    return np.sqrt(np.sum((arr2 - arr1) ** 2))
+
+
+def compute_multidimensional_distance(arr1, arr2):
+    return np.sqrt(np.sum((arr2 - arr1) ** 2))
+
+
+def compute_pair_distances(arr):
+    #return np.sqrt(np.sum((arr[:, None, :] - arr[None, :, :]) ** 2, axis=-1))
+    return np.linalg.norm(arr[:, None, :] - arr[None, :, :], axis=-1)
+
 
 if __name__== '__main__':
-    print(np.array([[1,2,3], [4,5,6], [7,8,9]]))
-    print(np.linspace(0, 3, 9))
-    print(np.arange(1, 9, 0.5))
+
+    np.array([[1,2,3], [4,5,6], [7,8,9]])
+    np.linspace(0, 3, 9)
+    np.arange(1, 9.5, 0.5)
 
 
+    # Test arrays and functions
 
+    # matrix_1 = np.array([[1,3], [2,4], [5,8]])
+    # matrix_2 = np.array([[1,4,6,7], [2,2,4,1]])
+    # print(matrix_multiplication(matrix_1, matrix_2))
 
-'''
-Основные задачи на оценку
-Создайте 3 ваших любимых эррея разными способами
-Создайте функцию matrix_multiplication, которая принимает 2 матрицы, перемножает их по соответствующим правилам и выдаёт получившуюся матрицу. Имеется ввиду матричное перемножение, а НЕ поэлементное
-Создайте функцию multiplication_check, которая принимает список с матрицами, и выдаёт True, если они могут быть перемножены друг на друга в порядке, в котором они находятся в списке, и False, если их нельзя перемножить.
-Создайте функцию multiply_matrices, которая принимает список с матрицами, и выдаёт результат перемножения, если его можно получить, или возвращает None, если их нельзя перемножить
-Создайте функцию compute_2d_distance, принимающую 2 одномерных эррея c парой значений (как координаты точки на плоскости) и вычисляющую расстояние между ними
-Создайте функцию compute_multidimensional_distance, принимающую 2 одномерных эррея с любым количеством значений (но равным) и вычисляющую расстояние между ними
-Создайте функцию compute_pair_distances, которая получает 2d эррей, где каждая строка это наблюдение, а каждый столбец - фича. Функция рассчитывает матрицу попарных расстояний и выдаёт её пользователю.
-Код вне функций писать не нужно (первое задание можно сделать это в блоке if __name__ == "__main__":). Все функции сохраните в файле numpy_challenge.py. Будет проводиться полуавтоматическая проверка, так что просим соблюдать указанный нейминг функций и файлов.
+    # matrix_list_true = [matrix_1, matrix_2, np.array([[1,3,5], [2,1,1], [3,4,7], [1,8,1]]), np.array([[2,2], [1,2], [2,2]])]
+    # matrix_list_false = [matrix_1, matrix_2, np.array([[1,3,5], [2,1,1], [3,4,7]]), np.array([[2,2], [1,2], [2,2]])]
+    # print(multiplication_check(matrix_list_false))
+    # print(multiply_matrices(matrix_list_false))
 
-Можно пользоваться любым функционалом библиотеки numpy. Другими модулями пользоваться нельзя. Если какая-то из функций numpy способна почти полностью решить задание, то её стоит использовать, в этом ДЗ крайне не рекомендуется изобретать велосипеды. Так что перед выполнением задания стоит ознакомиться с тем, что вообще умеет делать эта либа.
-'''
+    # arr1 = np.array([37,44])
+    # arr2 = np.array([5,16])
+    # print(compute_2d_distance(arr1, arr2))
+    # print(compute_multidimensional_distance(arr1, arr2))
+
+    # arr = np.array([[37, 44, 5], [11, 8, 13], [1,4,7], [2,2,5]])
+    # arr = np.array([[0,0],[1,1],[2,2]])
+    # print(compute_pair_distances(arr))
